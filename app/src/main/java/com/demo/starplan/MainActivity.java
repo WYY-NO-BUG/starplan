8f8cefd4d74dd8385af48d2e884b1980057f6987
林秋城 <565718255@qq.com>
2020-07-21 09:23:39 +0800

    初始化

--- app/src/main/java/com/demo/starplan/MainActivity.java
+++ app/src/main/java/com/demo/starplan/MainActivity.java
@@ -1,14 +1,8 @@
 package com.demo.starplan;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-import com.demo.starplan.ruiguang.RGActivity;
-import com.demo.starplan.wangxin.WXActivity;
-import com.demo.starplan.yueying.YYActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,25 +11,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, WXActivity.class));
-            }
-        });
-
-        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, YYActivity.class));
-            }
-        });
-
-        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, RGActivity.class));
-            }
-        });
     }
 }
