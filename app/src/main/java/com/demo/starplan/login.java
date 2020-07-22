5456eca4aae87a887368ac0aa232b581b688e9bc
WYY_NO_BUG <237914424@qq.com>
2020-07-22 16:09:48 +0800

    实现底部四个tab

---
+++ app/src/main/java/com/demo/starplan/login.java
@@ -0,0 +1,13 @@
+package com.demo.starplan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/com/demo/starplan/mine.java
+++ app/src/main/java/com/demo/starplan/mine.java
@@ -5,17 +5,29 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class mine extends Fragment {
+    private Button button;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_mine,container,false);
+        View view = inflater.inflate(R.layout.activity_mine, container, false);
+        button = view.findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mine.this,);
+            }
+        });
+
+        return view;
     }
 }
