4ba5d8936c83d372566415d098432445b5c16504
WYY_NO_BUG <237914424@qq.com>
2020-07-25 22:32:30 +0800

    viewpager+viewpager嵌套

--- app/src/main/java/com/demo/starplan/goodstudy/ReconmendedFragment.java
+++ app/src/main/java/com/demo/starplan/goodstudy/ReconmendedFragment.java
@@ -1,24 +1,35 @@
+
 package com.demo.starplan.goodstudy;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.demo.starplan.R;
 
+import java.util.ArrayList;
+
 
 public class ReconmendedFragment extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_recommended,container,false);
     }
+
+
 }
