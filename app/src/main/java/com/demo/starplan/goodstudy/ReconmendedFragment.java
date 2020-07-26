8bf73639b3e83718978b891f33d3296fa4c7b760
WYY_NO_BUG <237914424@qq.com>
2020-07-26 23:00:55 +0800

    实现recycleview的复杂多类型布局

--- app/src/main/java/com/demo/starplan/goodstudy/ReconmendedFragment.java
+++ app/src/main/java/com/demo/starplan/goodstudy/ReconmendedFragment.java
@@ -18,9 +18,12 @@ import android.widget.LinearLayout;
 import com.demo.starplan.R;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class ReconmendedFragment extends Fragment {
+    private View view;
+    private List<babybus> babybusList = new ArrayList<>();
 
     @Nullable
     @Override
@@ -28,8 +31,48 @@ public class ReconmendedFragment extends Fragment {
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_recommended,container,false);
+        view = inflater.inflate(R.layout.fragment_recommended, container, false);
+        initApp();
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
+//        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+//        recyclerView.setLayoutManager(layoutManager);
+        GridLayoutManager layoutManager = new GridLayoutManager(getContext(),20);
+
+        //spancount/spansize = 列数
+        layoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                babybus bbb = babybusList.get(position);
+                if (bbb.type== babybus.TypeOne){
+                    return 20;
+                }else if (bbb.type == babybus.TypeTwo){
+                    return 4;
+                }else if (bbb.type== babybus.TypeThree){
+                    return 20;
+                }else if (bbb.type == babybus.TypeFour){
+                    return 10;
+                }else return 0;
+            }
+        });
+        recyclerView.setLayoutManager(layoutManager);
+        BabybusAdapter adapter = new BabybusAdapter(babybusList,getContext());
+        recyclerView.setAdapter(adapter);
+
+        return view;
     }
 
+    public void initApp() {
 
+        babybus one = new babybus("儿歌", R.drawable.iv_cartoon_normal,1);
+        babybusList.add(one);
+        babybus t = new babybus("推荐", R.drawable.iv_cartoon_normal,4);
+        babybusList.add(t);
+        babybus q = new babybus("儿歌", R.drawable.iv_cartoon_normal,4);
+        babybusList.add(q);
+        babybus w = new babybus("儿歌", R.drawable.iv_cartoon_normal,4);
+        babybusList.add(w);
+        babybus e = new babybus("儿歌", R.drawable.iv_cartoon_normal,4);
+        babybusList.add(e);
+
+    }
 }
